64dd77f46bf4899d0032f39e0fb783ec45cd35d2
Daniel Iong <daniong@daniong-mbp.local>
2020-06-04 18:28:39 -0700

    changed BIC function

--- R/selectModel_BIC.R
+++ R/selectModel_BIC.R
@@ -2,6 +2,7 @@ selectModel_BIC = function(X, Y, seX, seY, K_range = 1:3, Nreps = 20,
                        verbose=FALSE) {
   
   p = length(X)
+  Q_vec = rep(NA, max(K_range))
   BIC_vec = rep(NA, max(K_range))
   
   for (K in K_range) {
@@ -11,9 +12,14 @@ selectModel_BIC = function(X, Y, seX, seY, K_range = 1:3, Nreps = 20,
     ## Run MC-EM with optimized initial values
     MCEM_fit = MR_EM(K, initVals, X, Y, seX, seY, saveTraj=FALSE, computeSE=FALSE)
     
-    BIC_vec[K] =  MCEM_fit$convergenceInfo$completeDataLogLik - (3*K * log(p))
+    Q_vec[K] = MCEM_fit$convergenceInfo$completeDataLogLik
+    BIC_vec[K] =  Q_vec[K] - (3*K * log(p))
   }
   
   K_opt = which.max(BIC_vec)
-  return(K_opt)  
+  return(list("K_opt" = K_opt,
+              Q = Q_vec,
+              BIC = BIC_vec
+              )
+         ) 
 }
